e9849706c7ddbd0674f951d5ee2861e39e2a42e7
tkaniowski <tk@piingu.dk>
2017-01-30 18:11:17 +0000

    Fixed menu order

--- modules/mod_mainmenucssimage/helper.php
+++ modules/mod_mainmenucssimage/helper.php
@@ -12,7 +12,7 @@ class modMainmenucssimageHelper
 	{
 	  $db =& JFactory::getDBO();
 	  $r = null;
-	  $query = "SELECT id,title,link FROM #__menu WHERE menutype = 'mainmenu' AND published=1";// ORDER BY ordering";
+	  $query = "SELECT id,title,link FROM #__menu WHERE menutype = 'mainmenu' AND published=1  ORDER BY lft";
 	  $db->setQuery($query);
 	  $r = $db->loadObjectList();
 	  return $r;
