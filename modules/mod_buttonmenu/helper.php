0aeef07ecc2935f3f41e1b71f28842b52f35b8e3
tkaniowski <tk@piingu.dk>
2017-03-16 17:10:00 +0000

    Button menu fix

--- modules/mod_buttonmenu/helper.php
+++ modules/mod_buttonmenu/helper.php
@@ -12,7 +12,7 @@ class modButtonmenuHelper
 	{
 	  $db =& JFactory::getDBO();
 	  $r = null;
-	  $query = "SELECT id,title,link FROM #__menu WHERE menutype = 'buttons_menu' AND published=1 ORDER BY ordering";
+	  $query = "SELECT id,title,link FROM #__menu WHERE menutype = 'buttons_menu' AND published=1 ORDER BY lft";
 	  $db->setQuery($query);
 	  $r = $db->loadObjectList();
 	  return $r;
