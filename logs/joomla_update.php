d4f8d5ecbd51fc39ab9f3aa4c0d37efc9ee1d880
bazylianie <bazylianie@ssh.dhosting.pl>
2018-12-18 00:01:35 +0100

    Deleted files after update to 3.9

--- logs/joomla_update.php
+++
@@ -1,8 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2018-11-03 16:42:39 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2018-11-03T16:42:39+00:00	INFO 5.172.255.15	update	Czyszczenie po instalacji.
-2018-11-03T16:42:39+00:00	INFO 5.172.255.15	update	Aktualizacja do wersji 3.9.0 zakończona.
